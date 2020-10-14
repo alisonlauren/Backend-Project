d83cdf4dce77b22faf3ee4e568e5cc8f8fb0102e
Alison Manning <alisonlaurenmanning@gmail.com>
2020-10-14 15:19:55 -0500

    worked on register.html

--- app.js
+++ app.js
@@ -27,3 +27,64 @@ app.use(express.static('./public'));
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
+
+
+function checkAuth(req, res, next) {
+if (req.session.user) {
+    next();
+} else {
+    res.redirect('/login');
+}
+}
+
+app.get('/', checkAuth, (req, res) => {
+res.render('login', {
+    locals: {
+    user: req.session.user
+    }
+});
+})
+
+//creating register page
+app.get('/register', (req, res) => {
+res.render('register', {
+locals: {
+    error: null,
+    title: "Register",
+
+},
+    partials: {head:"partials/head", footer: "partials/footer"},
+
+});
+})
+
+app.post('/register', (req, res) => {
+//check if post was submitted with email and password
+if (!req.body.email || !req.body.password) {
+    res.render('register', {
+    locals: {
+        error: "Please submit all required fields"
+    }
+    })
+    return;
+}
+const { email, password } = req.body;
+bcrypt.hash(password, 10, (err, hash) => {
+    db.User.create({
+    email: email,
+    password: hash,
+    })
+    .then((user) => {
+    res.redirect('/login');
+    })
+})
+
+})
+
+
+
+app.listen(3000, function () {
+    console.log('Activity Tracker API....');
+});
+
+
