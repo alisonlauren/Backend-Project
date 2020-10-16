b797c6dabd683fb406975882eb6a17fee85f63e1
Pete Looney <petelooney81@gmail.com>
2020-10-16 14:16:47 -0500

    before migration fixes, added challenges template and routes

--- models/user.js
+++ models/user.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Workout);
+      User.belongsToMany(models.Challenge, {through: 'UserChallenge'});
     }
   };
   User.init({
---
+++ routes/challenges.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+const checkAuth = (req, res, next) => {
+    if(req.session.user){
+        next();
+    }else{
+        res.render('login', {
+            locals: {
+                title: 'Login',
+                error: 'Must be logged in to access profile page'
+              },
+              partials: {
+                head: 'partials/head',
+                footer: 'partials/footer'
+              }
+        });
+    }
+}
+
+router.get('/', checkAuth, (req, res)=>{
+    res.render('challenge', {
+        locals: {
+            user: req.session.user,
+            title: "Challenges",
+            error: ''
+        },
+        partials: {
+            head:"partials/head",
+            footer: "partials/footer"
+        }
+    })
+})
+
+router.post('/', (req, res)=>{
+    const {workoutType, startTime, endTime, calorie, miles} = req.body
+    db.User.findOne({
+        where: {
+            id: req.session.user.id
+        }
+    })
+        .then(user=>{
+            user.createChallenge({
+                type: workoutType,
+                criteria: {
+                    start_time: startTime,
+                    end_time: endTime,
+                    cal: calorie,
+                    distance: miles
+                }
+            })
+            .then(challenge=>{
+                res.redirect('challenges')
+            })
+        })
+        .catch(e=>{
+            console.log(e)
+        })
+})
